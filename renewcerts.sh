d6e9560d07297aae7fd40fbb8c4ac9840183fa33
Frnot <frnot@frnot.com>
2021-08-05 14:28:52 -0500

    quiet on inaction

--- renewcerts.sh
+++ renewcerts.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-#v0.6
+#v0.7
 
 CERT_SERVER=<user>@acme-dns.lab.frnot.com
 CERT_DIR="/etc/cockpit/ws-certs.d"
 CMD="cat $CERT_DIR/key.pem $CERT_DIR/cert.pem > $CERT_DIR/50-le.cert && systemctl restart cockpit"
 
+verbose=false
+force=false
+
 run() {
-        if [ ! -z $1 ] && [ $1 = "--force" ]; then
+        if [ "$force" = true ]; then
                 printf "\"--force\" specified, renewing certs.\n"
                 renew
         elif [ $(new_certs_available) = "TRUE" ]; then
-                printf "New cert found, renewing.\n"
+                if [ "$verbose" = true ]; then
+                        printf "New cert found, renewing.\n"
+                fi
                 renew
         else
-                printf "No new certificates found, exiting.\n"
+                if [ "$verbose" = true ]; then
+                        printf "No new certificates found, exiting.\n"
+                fi
         fi
 }
 
@@ -31,10 +38,30 @@ new_certs_available() {
 }
 
 renew() {
-        sftp $CERT_SERVER:/* $CERT_DIR
+        if [ "$verbose" = true ]; then
+                sftp $CERT_SERVER:/* $CERT_DIR
+        else
+                sftp $CERT_SERVER:/* $CERT_DIR >/dev/null
+        fi
 
         printf "Executing: $CMD\n"
         eval $CMD
 }
+
+while test $# -gt 0; do
+    case "$1" in
+        -f|--force)
+            force=true
+            shift
+            ;;
+        -v)
+            verbose=true
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
 
 run $@
